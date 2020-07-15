33565c16a53f7bc638b9ba36ac70756f2c83887b
VplateroIFIC <55190148+VplateroIFIC@users.noreply.github.com>
2020-07-15 15:38:43 +0200

    Update Dispensing_lines_time_measure.m

--- Dispensing_lines_time_measure.m
+++ Dispensing_lines_time_measure.m
@@ -1,31 +1,37 @@
 for i=1:10
-gluing.DispenseContinius('R0')
+gluing.DispenseContinius('R3S1')
 variable = genvarname(sprintf('timeStop%d',i))
-load('R0_times.mat', timeStop)
-eval([variable '  = timeStop'])
+load('R3S1_times.mat', 'timeStop')
+eval([variable ' = timeStop'])
 end
 %Hacemos una matriz con todos los 10 vectores conseguidos
 times = [timeStop1;timeStop2;timeStop3;timeStop4;timeStop5;timeStop6;timeStop7;timeStop8;timeStop9;timeStop10];
-save('R0_times.mat', 'times')
+save('R3S1_times.mat', 'times')
 
 media = mean(times);
-save('R0.mat', times)
+timeStop = media;
+save('R3S1.mat', 'timeStop')
 
-gantry.MoveToFast(350,0)
-gantry.MoveTo(gantry.Z1,-25,10)
-gantry.MoveTo(gantry.Z2,-65,10)
+return
+
+gantry.MoveToFast(0,400)
+gantry.WaitForMotionAll();
+gantry.MoveTo(gantry.Z1,-25,5)
+gantry.MoveTo(gantry.Z2,-65,5)
+gantry.WaitForMotionAll();
 gantry.MotorDisableAll;
 gantry.Disconnect;
 
 return
 
-for i=1:28
+
+for i=1:length( timeStop )
     timeStop(i) = mean(times(i));
 end
 
 return
 
-for i=1:10
-variable = genvarname(sprintf('timeStop%d',i))
-eval([variable '  = i*2'])
-end
+% for i=1:10
+% variable = genvarname(sprintf('timeStop%d',i))
+% eval([variable '  = i*2'])
+% end
